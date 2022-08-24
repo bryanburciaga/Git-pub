e817ddfa272202f2d31e076488846a5c1d633a39
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-08-24 00:15:51 -0600

    so far

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 // dependencies
 const express = require('express');
 const port = 3000;
+// to read valus from .env files
+//  and the process.env
+const drinks = require('./models/drinks');
+// console.log('this is drinks inside of drinks.js', drinks);
 
 const app = express();
 
 app.get('/drinks', (req, res) => {
-    res.send('Welcome to the Gitpub App!');
+    res.render('drinks_index.ejs', {drinks: drinks});
 });
+
+app.get('/drinks/:id', (req, res) => {
+    const foundDrinks = drinks.find(function(f) {
+        return f.id == req.params.id
+    });
+    res.render('drinks_show.ejs', {drink: foundDrinks});
+});
+
 
 app.listen(port, () => {
     console.log(`Im in port ${port}.`);
